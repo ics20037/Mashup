f98a1b2153609b1ff97921eefd2f66d53cb0f5c2
DESKTOP-T7Q3D7F\THISisME <kostasgianprod@gmail.com>
2021-05-07 19:14:47 +0300

    Created Post and base.html (And made migrations)

--- Mashup_Project/Mashup/models.py
+++ Mashup_Project/Mashup/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    class Content():
+        images = models.ImageField()
+        videos = models.FileField()
+        text = models.TextField()
+    content = Content()
