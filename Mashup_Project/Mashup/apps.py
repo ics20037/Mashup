07e33fe592f91230b698cf793836d59036868203
aris <dai18027@uom.edu.gr>
2021-05-09 18:31:22 +0300

    added profile

--- Mashup_Project/Mashup/apps.py
+++ Mashup_Project/Mashup/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MashupConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Mashup'
+
+    def ready(self):
+        import Mashup.signals
