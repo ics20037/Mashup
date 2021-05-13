602bafa5fbc8fbfa094f54fea69255d62d78f2b7
aris <dai18027@uom.edu.gr>
2021-05-13 21:59:26 +0300

    -Added User functionality

--- Mashup_Project/Mashup/urls.py
+++ Mashup_Project/Mashup/urls.py
@@ -1,7 +1,11 @@
-from . import views
+from . import views as mashup_views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('register', views.register, name='register'),
+    path('', mashup_views.index, name='index'),
+    path('register/', mashup_views.register, name='register'),
+    path('profile/', mashup_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='Mashup/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Mashup/logout.html'), name='logout'),
 ]
--- Mashup_Project/Mashup/views.py
+++ Mashup_Project/Mashup/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, 'mashup\index.html')
 
 def register(request):
-    return render(request, 'mashup\register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'mashup/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'mashup/profile.html')
