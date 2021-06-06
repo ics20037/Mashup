10542adb09c47e5fb3cdfc30fbece3a1553bf07f
DESKTOP-T7Q3D7F\THISisME <kostasgianprod@gmail.com>
2021-06-06 15:52:51 +0300

    Removed some migrations and models

--- Mashup_Project/Mashup/models.py
+++ Mashup_Project/Mashup/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-    class Content():
-        images = models.ImageField()
-        videos = models.FileField()
-        text = models.TextField()
-    content = Content()
