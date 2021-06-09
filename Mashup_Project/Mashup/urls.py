05276f318d6ababdaaffd0549fe84f260f0f7645
aris <37934173+aris865@users.noreply.github.com>
2021-06-09 19:31:15 +0300

    test

--- Mashup_Project/Mashup/urls.py
+++ Mashup_Project/Mashup/urls.py
@@ -9,3 +9,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='Mashup/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='Mashup/logout.html'), name='logout'),
 ]
+
+##test
